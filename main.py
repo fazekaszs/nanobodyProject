1f278b22886c9dc20361a78160edfd3c867e5988
fazekaszs <zsolt.fazekas.9@gmail.com>
2023-10-21 18:07:12 +0200

    A lot of improvements in plotting. Group separations are now depicted with density plots and ROC curves instead of cross-scatter plots.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Union
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,6 +7,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 from sklearn.neighbors import KernelDensity
+from sklearn.metrics import roc_curve, auc
 from data_readers import read_mutabind2, read_saambe_3d, read_mcsm_ppi2
 
 PLOT_DIR = Path("plots")
@@ -25,10 +28,12 @@ def get_statistics(table: pd.DataFrame):
         "Mean": table.mean(),
         "StD": table.std(),
         "Median": (t_med := table.median()),
-        "MAD": (table - t_med).abs().median()
+        "MAD": (table - t_med).abs().median(),
+        "Sarle\'s bimodality coefficient": (table.skew() ** 2 + 1) / (table.kurt() + 3),
+        "Percentage greater than 0": table.map(lambda x: 100 * int(x > 0)).sum() / table.shape[0]
     }, axis=1)
 
-    print(table_stats, "\n")
+    print(table_stats.to_string(), "\n")
 
     t_ranks = table.rank(axis=0)
     print(t_ranks)
@@ -40,113 +45,45 @@ def get_statistics(table: pd.DataFrame):
     print(table.corr(method="spearman"), "\n")
 
 
-def series_to_density(ser: pd.Series):
+def series_to_density(
+        ser: Union[np.ndarray, pd.Series],
+        min_value=None,
+        max_value=None,
+        n_of_point=None
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Creates density datapoints with KDE from a series of data.
 
-    :param ser: The Pandas Series instance
+    :param ser: A 1D Pandas Series instance or a numpy ndarray instance.
+    :param min_value: The minimum value for the x range.
+    :param max_value: The maximum value for the x range.
+    :param n_of_point: The number of points in the x range.
     :return: The x and y values of the datapoints.
     """
 
-    min_value = ser.min()
-    max_value = ser.max()
-    n_of_points = 1000
+    if type(ser) is pd.Series:
+        ser: np.ndarray = ser.to_numpy()
+
+    min_value = np.min(ser) if min_value is None else min_value
+    max_value = np.max(ser) if max_value is None else max_value
+    n_of_points = 1000 if n_of_point is None else n_of_point
+
     x_values = np.arange(min_value, max_value, (max_value - min_value) / n_of_points)
 
     kde = KernelDensity(bandwidth="scott")
-    kde.fit(ser.to_numpy()[:, np.newaxis])
+    kde.fit(ser[:, np.newaxis])
     y_values = kde.score_samples(x_values[:, np.newaxis])
 
     return x_values, np.exp(y_values)
 
 
-def categoric_mutations(ser: pd.Series, categories: str):
+def plot_distributions(table: pd.DataFrame):
     """
-    Filters the mutations inside a series based on whether they are "in cat -> not in cat" (green)
-    or "not in cat -> cat" (blue) mutations. "in cat -> in cat" and "not in cat -> not in cat" mutations
-    are thrown away. If the categories string is empty, it will simply return the values with the black color
-    vector.
+    Creates plots that help t visualize the distribution of the predicted ddG values.
 
-    :param ser: The Pandas Series instance
-    :param categories: The OLC of the amino acids inside the specific category.
-    :return: The filtered values and the colors associated with the mutation direction.
+    :param table:
+    :return:
     """
-
-    if categories == "":
-        return ser.to_numpy(), np.array(["black" for _ in ser])
-
-    colors = list()
-    for mut in ser.index:
-
-        mut = mut.split(":")[1]
-        wt_in_cat = mut[0] in categories
-        mut_in_cat = mut[-1] in categories
-
-        if wt_in_cat and not mut_in_cat:
-            colors.append("green")
-        elif not wt_in_cat and mut_in_cat:
-            colors.append("blue")
-        else:
-            colors.append(0)
-
-    colors = np.array(colors, dtype=object)
-    mask = colors != 0
-    values = ser.to_numpy()[mask]
-    colors = colors[mask]
-
-    return values, colors
-
-
-def plot_categoric_mutations(table: pd.DataFrame, categories: str):
-
-    fig, ax = plt.subplots(1, 3)
-    fig.suptitle(
-        "Predicted $\\Delta\\Delta G$ values in $\\mathrm{kcal} \\cdot \\mathrm{mol}^{-1}$ dimension\n"
-        "for the different predictors"
-    )
-    fig.subplots_adjust(wspace=0.3)
-    fig.set_size_inches(13, 5)
-
-    for axis in ax:
-        axis.set_aspect("equal")
-        axis.grid(visible=True, linestyle="dashed")
-        axis.set_axisbelow(True)
-
-    t1, colors = categoric_mutations(table["mCSM-PPI2"], categories)
-    t2, _ = categoric_mutations(table["MutaBind2"], categories)
-    t3, _ = categoric_mutations(table["SAAMBE-3D"], categories)
-
-    ax[0].scatter(t1, t2, marker=".", c=colors, alpha=0.5, edgecolors=None)
-    ax[1].scatter(t1, t3, marker=".", c=colors, alpha=0.5, edgecolors=None)
-    ax[2].scatter(t2, t3, marker=".", c=colors, alpha=0.5, edgecolors=None)
-
-    ax[0].set_xlabel("mCSM-PPI2")
-    ax[0].set_ylabel("MutaBind2")
-
-    ax[1].set_xlabel("mCSM-PPI2")
-    ax[1].set_ylabel("SAAMBE-3D")
-
-    ax[2].set_xlabel("MutaBind2")
-    ax[2].set_ylabel("SAAMBE-3D")
-
-    fig_name = "cross_scatter.png" if categories == "" else f"cross_scatter_{categories}.png"
-
-    fig.savefig(PLOT_DIR / fig_name, dpi=300)
-
-
-def plot_data(table: pd.DataFrame):
-
-    plt.rcParams.update({"font.size": 14})
-
-    # Create cross-scatter plots
-
-    plot_categoric_mutations(table, "")
-    plot_categoric_mutations(table, "FWYH")
-    plot_categoric_mutations(table, "RHKDESTNQ")
-    plot_categoric_mutations(table, "RK")
-    plot_categoric_mutations(table, "DE")
-    plot_categoric_mutations(table, "P")
-    plot_categoric_mutations(table, "G")
 
     # Create boxplot
 
@@ -202,15 +139,196 @@ def plot_data(table: pd.DataFrame):
     fig.savefig(PLOT_DIR / "densities.png", dpi=300)
 
 
+def categoric_mutations(ser: pd.Series, category_resi: str) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Filters the mutations inside a series based on whether they are "in cat -> not in cat" (out of category)
+    or "not in cat -> cat" (into category) mutations.
+    "in cat -> in cat" and "not in cat -> not in cat" mutations are thrown away.
+
+    :param ser: The Pandas Series instance
+    :param category_resi: The OLC of the amino acids inside the specific category.
+    :return: The filtered values and the "out of category mutation" property (mask) of those values.
+    """
+
+    values = list()
+    out_of_cat = list()
+    for ser_idx in ser.index:
+
+        mut = ser_idx.split(":")[1]
+        wt_in_cat = mut[0] in category_resi
+        mut_in_cat = mut[-1] in category_resi
+
+        if wt_in_cat and not mut_in_cat:
+            values.append(ser[ser_idx])
+            out_of_cat.append(True)
+        elif not wt_in_cat and mut_in_cat:
+            values.append(ser[ser_idx])
+            out_of_cat.append(False)
+
+    return np.array(values, dtype=float), np.array(out_of_cat, dtype=bool)
+
+
+def categoric_densities(values: np.ndarray, mask: np.ndarray) -> Tuple[np.ndarray, ...]:
+
+    min_value = np.min(values)
+    max_value = np.max(values)
+
+    x_values, y1_values = series_to_density(values[mask], min_value, max_value)
+    _, y2_values = series_to_density(values[~mask], min_value, max_value)
+
+    return x_values, y1_values, y2_values
+
+
+def plot_correlation_scatter(table: pd.DataFrame) -> None:
+    """
+    Creates cross-scatter plots between the different predictors to view the correlation between them.
+
+    :param table: The Pandas DataFrame instance containing the predicted ddG values.
+    """
+
+    fig, ax = plt.subplots(1, 3)
+    fig.suptitle("Scatter plots between the different predictors to assess correlation")
+    fig.subplots_adjust(wspace=0.3)
+    fig.set_size_inches(13, 5)
+
+    for axis in ax:
+        axis.set_aspect("equal")
+        axis.grid(visible=True, linestyle="dashed")
+        axis.set_axisbelow(True)
+
+    ax[0].scatter(table["mCSM-PPI2"], table["MutaBind2"], color="black", marker=".", alpha=0.3, edgecolors=None)
+    ax[1].scatter(table["mCSM-PPI2"], table["SAAMBE-3D"], color="black", marker=".", alpha=0.3, edgecolors=None)
+    ax[2].scatter(table["MutaBind2"], table["SAAMBE-3D"], color="black", marker=".", alpha=0.3, edgecolors=None)
+
+    dim_str = "$\\Delta\\Delta G$ / $\\mathrm{kcal} \\cdot \\mathrm{mol}^{-1}$"
+
+    ax[0].set_xlabel(f"mCSM-PPI2 {dim_str}")
+    ax[0].set_ylabel(f"MutaBind2 {dim_str}")
+
+    ax[1].set_xlabel(f"mCSM-PPI2 {dim_str}")
+    ax[1].set_ylabel(f"SAAMBE-3D {dim_str}")
+
+    ax[2].set_xlabel(f"MutaBind2 {dim_str}")
+    ax[2].set_ylabel(f"SAAMBE-3D {dim_str}")
+
+    fig_name = "correlation_scatter.png"
+
+    fig.savefig(PLOT_DIR / fig_name, dpi=300)
+
+
+def plot_categoric_mutations(table: pd.DataFrame, category_resi: str):
+
+    t1, cat_mask = categoric_mutations(table["mCSM-PPI2"], category_resi)
+    t2, _ = categoric_mutations(table["MutaBind2"], category_resi)
+    t3, _ = categoric_mutations(table["SAAMBE-3D"], category_resi)
+
+    fig, ax = plt.subplots(1, 3)
+    fig.suptitle(f"Out of and into {category_resi} group separation of predictors")
+    fig.subplots_adjust(wspace=0.3, top=0.8)
+    fig.set_size_inches(14, 6)
+
+    x1_values, y1_out_values, y1_in_values = categoric_densities(t1, cat_mask)
+    ax[0].set_title("mCSM-PPI2")
+    ax[0].plot(x1_values, y1_out_values, color="blue", label="out of group")
+    ax[0].plot(x1_values, y1_in_values, color="red", label="into group")
+
+    x2_values, y2_out_values, y2_in_values = categoric_densities(t2, cat_mask)
+    ax[1].set_title("MutaBind2")
+    ax[1].plot(x2_values, y2_out_values, color="blue", label="out of group")
+    ax[1].plot(x2_values, y2_in_values, color="red", label="into group")
+
+    x3_values, y3_out_values, y3_in_values = categoric_densities(t3, cat_mask)
+    ax[2].set_title("SAAMBE-3D")
+    ax[2].plot(x3_values, y3_out_values, color="blue", label="out of group")
+    ax[2].plot(x3_values, y3_in_values, color="red", label="into group")
+
+    for axis in ax:
+
+        axis.set_xlabel("$\\Delta\\Delta G$ / $\\mathrm{kcal} \\cdot \\mathrm{mol}^{-1}$")
+        axis.set_ylabel("Density")
+        axis.legend(loc="upper center")
+        y_min, y_max = axis.get_ylim()
+        axis.set_ylim(y_min, 1.3 * y_max)
+
+    fig_name = f"categoric_separation_density_{category_resi}.png"
+
+    fig.savefig(PLOT_DIR / fig_name, dpi=300)
+
+    # Get ROC-AUC for the separation power of the given category
+
+    plt.close(fig)
+    fig, ax = plt.subplots(1, 3)
+    fig.suptitle(
+        f"ROC-AUC description of the classification ability of $\\Delta\\Delta G$ values\n"
+        f"with respect to into- and out of {category_resi} group mutations"
+    )
+    fig.subplots_adjust(wspace=0.4)
+    fig.set_size_inches(13, 5)
+
+    ax[0].set_title("mCSM-PPI2")
+    ax[1].set_title("MutaBind2")
+    ax[2].set_title("SAAMBE-3D")
+
+    tick_posi = np.arange(0, 1.2, 0.2)
+
+    for axis in ax:
+        axis.set_xlabel("false out of group rate"),
+        axis.set_ylabel("true out of group rate"),
+        axis.set_xticks(tick_posi),
+        axis.set_yticks(tick_posi),
+        axis.set_xlim(-0.05, 1.05),
+        axis.set_ylim(-0.05, 1.05),
+        axis.set_aspect("equal"),
+        axis.grid()
+
+    fpr1, tpr1, _ = roc_curve(np.array(cat_mask, dtype=int), t1)
+    fpr2, tpr2, _ = roc_curve(np.array(cat_mask, dtype=int), t2)
+    fpr3, tpr3, _ = roc_curve(np.array(cat_mask, dtype=int), t3)
+
+    auc1 = auc(fpr1, tpr1)
+    auc2 = auc(fpr2, tpr2)
+    auc3 = auc(fpr3, tpr3)
+
+    ax[0].plot(fpr1, tpr1, color="black")
+    ax[1].plot(fpr2, tpr2, color="black")
+    ax[2].plot(fpr3, tpr3, color="black")
+
+    bbox_style = {"boxstyle": "round", "facecolor": "white", "alpha": 0.5}
+    alignment_style = {"verticalalignment": "bottom", "horizontalalignment": "right"}
+    ax[0].text(0.95, 0.05, f"{auc1:.3f}", transform=ax[0].transAxes, bbox=bbox_style, **alignment_style)
+    ax[1].text(0.95, 0.05, f"{auc2:.3f}", transform=ax[1].transAxes, bbox=bbox_style, **alignment_style)
+    ax[2].text(0.95, 0.05, f"{auc3:.3f}", transform=ax[2].transAxes, bbox=bbox_style, **alignment_style)
+
+    fig_name = f"roc_auc_{category_resi}.png"
+
+    fig.savefig(PLOT_DIR / fig_name, dpi=300)
+
+
 def main():
+
+    # Data loading
 
     table = read_mcsm_ppi2()
     table = table.join(read_saambe_3d(), how="inner")
     table = table.join(read_mutabind2(), how="inner")
 
-    # get_statistics(table)
+    # Statistics calculation
 
-    plot_data(table)
+    get_statistics(table)
+
+    # Plotting
+
+    plt.rcParams.update({"font.size": 16})
+
+    plot_distributions(table)
+    plot_correlation_scatter(table)
+
+    plot_categoric_mutations(table, "FWYH")
+    plot_categoric_mutations(table, "RHKDESTNQ")
+    plot_categoric_mutations(table, "RK")
+    plot_categoric_mutations(table, "DE")
+    plot_categoric_mutations(table, "P")
+    plot_categoric_mutations(table, "G")
 
 
 if __name__ == "__main__":
