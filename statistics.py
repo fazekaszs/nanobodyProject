b8f20112ae29e3eeeb69498126aed23fcf5bfebd
fazekaszs <zsolt.fazekas.9@gmail.com>
2023-10-22 20:07:30 +0200

    Further, small improvements on plotting: all plotting functions are sourced out to a standalone script. Statistics calculation functions too. Added a new statistics calculation function with the name of get_rbd_statistics.

---
+++ statistics.py
@@ -0,0 +1,86 @@
+import pandas as pd
+
+
+def get_global_statistics(table: pd.DataFrame):
+
+    t_med = table.median()
+    t_mad = (table - t_med).abs().median()
+    t_z = (table - t_med) / t_mad
+    t_z = t_z.mean(axis=1)
+    t_z = t_z.sort_values()
+
+    table_stats = pd.concat({
+        "Minimum": (t_min := table.min()),
+        "Maximum": (t_max := table.max()),
+        "Range": t_max - t_min,
+        "Mean": table.mean(),
+        "StD": table.std(),
+        "Median": (t_med := table.median()),
+        "MAD": (table - t_med).abs().median(),
+        "Sarle\'s bimodality coefficient": (table.skew() ** 2 + 1) / (table.kurt() + 3),
+        "Percentage greater than 0": table.map(lambda x: 100 * int(x > 0)).sum() / table.shape[0]
+    }, axis=1)
+
+    print(table_stats.to_string(), "\n")
+
+    t_ranks = table.rank(axis=0)
+    print(t_ranks)
+
+    print("Pearson\'s correlation matrix:")
+    print(table.corr(method="pearson"), "\n")
+
+    print("Spearman\'s correlation matrix:")
+    print(table.corr(method="spearman"), "\n")
+
+
+def get_rbd_statistics(table: pd.DataFrame):
+
+    cols = [
+        "SD1:mCSM-PPI2", "SD2:mCSM-PPI2",
+        "SD1:MutaBind2", "SD2:MutaBind2",
+        "SD1:SAAMBE-3D", "SD2:SAAMBE-3D",
+    ]
+    rows = list()
+    values = list()
+
+    wt_filtered = table.filter(like="_SWT:", axis=0)
+
+    for swt_index in wt_filtered.index:
+
+        # Create new row name.
+
+        row_name = swt_index.replace("_SWT:", ":")
+
+        # Calculate dddG values:
+        # dddg_p[i]_sd[j] is the change in ddG predicted by the [i]th predictor (i = 1, 2, 3)
+        #  when we move from the WT S-RBD to the Delta[j] variant S-RBD (j = 1, 2).
+
+        sd1_index = swt_index.replace("_SWT:", "_SD1:")
+        sd2_index = swt_index.replace("_SWT:", "_SD2:")
+
+        if sd1_index not in table.index or sd2_index not in table.index:
+            continue
+
+        dddg_p1_sd1 = table["mCSM-PPI2"][sd1_index] - wt_filtered["mCSM-PPI2"][swt_index]
+        dddg_p1_sd2 = table["mCSM-PPI2"][sd2_index] - wt_filtered["mCSM-PPI2"][swt_index]
+
+        dddg_p2_sd1 = table["MutaBind2"][sd1_index] - wt_filtered["MutaBind2"][swt_index]
+        dddg_p2_sd2 = table["MutaBind2"][sd2_index] - wt_filtered["MutaBind2"][swt_index]
+
+        dddg_p3_sd1 = table["SAAMBE-3D"][sd1_index] - wt_filtered["SAAMBE-3D"][swt_index]
+        dddg_p3_sd2 = table["SAAMBE-3D"][sd2_index] - wt_filtered["SAAMBE-3D"][swt_index]
+
+        rows.append(row_name)
+        values.append([dddg_p1_sd1, dddg_p1_sd2, dddg_p2_sd1, dddg_p2_sd2, dddg_p3_sd1, dddg_p3_sd2])
+
+    dddg_table = pd.DataFrame(data=values, index=rows, columns=cols)
+
+    # Print statistics
+    for col_name in cols:
+
+        current_table = dddg_table[col_name].sort_values()
+
+        print(f"\nvariant:predictor = {col_name}")
+
+        print("- HEAD -\n" + current_table.head(n=60).to_string())
+        print("- TAIL -\n" + current_table.tail(n=60).to_string())
